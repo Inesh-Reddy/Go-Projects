e011c72a22bcf87896a915fd037eb1be7af84709
Inesh-Reddy <ineshreddy007@gmail.com>
2025-01-09 22:19:37 +0530

    rewritten hellohandler API

--- 01-web-server/main.go
+++ 01-web-server/main.go
@@ -20,14 +20,14 @@ func formHandler(w http.ResponseWriter, r *http.Request){
 
 func helloHandler(w http.ResponseWriter, r *http.Request){
 	if r.URL.Path != "/hello" {
-		http.Error(w, "404 not Found", http.StatusNotFound)
+		http.Error(w, "404 Not Found", http.StatusNotFound)
 		return
 	}
-	if r.Method != "GET"{
-		http.Error(w, "method is not supported", http.StatusNotFound)
+	if r.Method != "GET" {
+		http.Error(w, "Wrong method", http.StatusNotFound)
 		return
 	}
-	fmt.Fprintf(w, "hello!")
+	fmt.Fprintf(w, "Hello!!, Welcome to land of Golanguage.")
 }
 
 func main(){
